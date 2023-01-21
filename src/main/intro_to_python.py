7cb8b60246d7fc6f16127726e27adb0e92497e13
ronitagarwala01 <34790361+ronitagarwala01@users.noreply.github.com>
2023-01-20 22:40:46 -0500

    Update intro_to_python.py

--- src/main/intro_to_python.py
+++ src/main/intro_to_python.py
@@ -1,1 +1,24 @@
+import numpy as np
 
+if __name__ == "__main__":
+    arr = np.zeros((3, 3)) #Create 3x3 matrix with all 0s
+    
+    #Solution for 1; Prints 1s where row number == column number
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr[i])):
+            if i==j:
+                arr[i][j] = 1
+    print(arr)
+    print()
+
+    #Solution for 2; Adds 3 to elements where row number != column number
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr[i])):
+            if i!=j:
+                arr[i][j] += 3
+    print(arr)
+    print()
+
+    #Solution for 3; Deletes last column
+    arr = np.delete(arr, obj = 2, axis = 1)
+    print(arr)
